4f93cb1d547dd7dda0a345e15da19ba9ce1a5e6e
Matthias Grieder <matthiasgrieder@gmail.com>
2022-02-15 21:04:23 +0100

    Update wrapRootElement.js

--- wrapRootElement.js
+++ wrapRootElement.js
@@ -2,7 +2,10 @@
 import React from "react";
 import { Location } from "@reach/router";
 import { MDXProvider } from "@mdx-js/react";
+import { IntercomProvider } from "react-use-intercom";
 import components from "@/components/mdx";
+
+const INTERCOM_APP_ID = 'kk4zxl1p';
 
 const App = ({ element, location }) => {
   return <MDXProvider {...{ components }}>{element}</MDXProvider>;
@@ -11,7 +14,9 @@ const App = ({ element, location }) => {
 const wrapRootElement = ({ element }) => (
   <Location>
     {(location) => (
+      <IntercomProvider appId={INTERCOM_APP_ID} autoBoot initializeDelay={500}>
         <App {...{ element }} {...location} />
+      </IntercomProvider>
     )}
   </Location>
 );
