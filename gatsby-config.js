34d569237ff6c9a00049f6ae200e47b8ce82a136
Matthias Grieder <matthiasgrieder@gmail.com>
2022-02-24 20:15:50 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,8 +26,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
-        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
         enableTags: true,
       },
     },
