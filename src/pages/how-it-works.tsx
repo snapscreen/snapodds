fad84c915230b816a755b0a2467f82146c02871c
Matthias Grieder <matthiasgrieder@gmail.com>
2022-05-03 09:28:52 +0200

    Update how-it-works.tsx
    hide Try now link

--- src/pages/how-it-works.tsx
+++ src/pages/how-it-works.tsx
@@ -13,9 +13,11 @@ const HowItWorks: React.FC<PageProps> = ({ data, location }) => {
         title="Display odds with just a snap of the live TV screen."
         lead="SnapOdds is a revolutionary bet discovery and engagement tool from Snapscreen. Integrate into your web and mobile experiences and acquisition path to create a dynamic, frictionless path to wagering."
       >
+        {/*
         <Button as="link" to="/try-now" styleType="primary">
           Try now
         </Button>
+        */}
       </Hero>
       <Container>
         <div
