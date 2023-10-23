0e4517c9da448860b7684ed8e92430d2d9f20a01
Matthias Grieder <matthias.grieder@snapscreen.com>
2023-10-23 18:09:45 +0200

    add new client logo

--- src/components/LogoCloud/index.tsx
+++ src/components/LogoCloud/index.tsx
@@ -12,13 +12,14 @@ export const LogoCloud: React.FC<EmptyProps> = () => {
           <h2 className="mx-auto text-xl font-bold mt-6 mb-12">
             Our B2B products are for sportsbooks, affiliates and sports media.
           </h2>
-          <div className="mx-auto grid max-w-lg grid-cols-2 items-center gap-x-8 gap-y-12 sm:mb-8 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-6">
+          <div className="mx-auto grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:mb-8 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-4">
             <img className="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="../clients/betika.webp" alt="Betika" width="158" height="48" />
             <img className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 dark:invert" src="../clients/lottomatica.svg" alt="Lottomatica" width="158" height="48" />
             <img className="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="../clients/Fox_Sports.svg" alt="Fox Sports" width="158" height="48" />
             <img className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 dark:invert" src="../clients/maxxwin.svg" alt="Maxxwin" width="158" height="48" />
             <img className="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="../clients/Netball_Australia.svg" alt="Netball Australia" width="158" height="48" />
             <img className="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="../clients/StationsCasino.png" alt="Stations Casino" width="158" height="48" />
+            <img className="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="../clients/wbsc-logo.svg" alt="WBSC - World Baseball Softball Confederation" width="158" height="48" />
           </div>
         </div>
       </Container>
