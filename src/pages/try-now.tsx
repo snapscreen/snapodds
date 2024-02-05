bb5445079556b1e446964ddbba0612afd131f148
Matthias Grieder <matthias.grieder@snapscreen.com>
2024-02-05 13:15:45 +0100

    Update try-now.tsx

--- src/pages/try-now.tsx
+++ src/pages/try-now.tsx
@@ -20,10 +20,10 @@ const TryNow: React.FC<PageProps> = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      {/*
       <div className="backdrop-blur-lg fixed inset-0 flex z-20 h-screen top-20">
         <h1 className="text-center mx-auto mt-24 font-bold text-4xl max-w-lg">Sorry, our demo is currently in maintainance and offline.</h1>
       </div>
+      {/*
       */}
       <Seo title="See how it works" />
       <Hero
